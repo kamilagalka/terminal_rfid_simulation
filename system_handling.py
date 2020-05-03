405130d236fc1fc63082a3515dd4becc50b9f2bd
kamilagalka <contactkamilagalka@gmail.com>
2020-05-03 19:02:17 +0200

    modified user interface

--- system_handling.py
+++ system_handling.py
@@ -5,9 +5,9 @@ import server
 
 window = tkinter.Tk()
 
-list_of_terminals = tkinter.Listbox(window, height=7, exportselection=0)
+list_of_terminals = tkinter.Listbox(window, height=7, width=40, exportselection=0)
 list_of_cards = tkinter.Listbox(window, height=7, width=40, exportselection=0)
-list_of_workers = tkinter.Listbox(window, height=7, exportselection=0)
+list_of_workers = tkinter.Listbox(window, height=7, width=40, exportselection=0)
 
 
 def add_new_terminal(new_terminal_id):
@@ -158,15 +158,15 @@ def fill_list_of_workers():
 
 
 def create_main_window():
-    window.geometry("670x520")
+    window.geometry("750x550")
     window.title("System Handling")
 
-    tkinter.Label(window, text="Choose object and action", font=("Courier", 13), fg="green").grid(
+    tkinter.Label(window, text="Choose object and action", font=("Courier", 13)).grid(
         column=1)
 
-    tkinter.Label(window, text="Terminals: ", font=("Courier", 10), fg="blue").grid(row=1, column=0)
-    tkinter.Label(window, text="Cards: ", font=("Courier", 10), fg="blue").grid(row=1, column=1)
-    tkinter.Label(window, text="Workers: ", font=("Courier", 10), fg="blue").grid(row=1, column=2)
+    tkinter.Label(window, text="Terminals: ", font=("Courier", 10)).grid(row=1, column=0)
+    tkinter.Label(window, text="Cards: ", font=("Courier", 10)).grid(row=1, column=1)
+    tkinter.Label(window, text="Workers: ", font=("Courier", 10)).grid(row=1, column=2)
 
     fill_list_of_terminals()
     fill_list_of_cards()
@@ -174,79 +174,76 @@ def create_main_window():
 
     # --------------------------------------------------------------------------------------------------------
 
-    tkinter.Label(window, text="Add new terminal", font=("Courier", 8), fg="blue").grid(row=3, column=0)
-    tkinter.Label(window, text="Insert terminal ID\nformat: T<number>", font=("Courier", 8)).grid(row=4,
-                                                                                                  column=0)
+    tkinter.Label(window, text="Connect or disconnect\nchosen terminal", font=("Courier", 10), fg="blue").grid(
+        row=3, column=0)
+    tkinter.Button(window, text="Connect", fg="blue",
+                   command=lambda: connect_terminal(list_of_terminals.get(tkinter.ANCHOR).split(" - ")[0])).grid(
+        row=4, column=0)
+    tkinter.Button(window, text="Disconnect", fg="blue",
+                   command=lambda: disconnect_terminal(list_of_terminals.get(tkinter.ANCHOR).split(" - ")[0])).grid(
+        row=5, column=0)
+
+    tkinter.Label(window, text="Add new terminal", font=("Courier", 10), fg="green").grid(row=7, column=0)
+    tkinter.Label(window, text="Insert terminal ID\nformat: T<number>", font=("Courier", 8), fg="green").grid(row=8,
+                                                                                                              column=0)
     terminal_entry = tkinter.Entry(window)
-    terminal_entry.grid(row=5, column=0)
-    tkinter.Button(window, text="Add terminal",
-                   command=lambda: add_new_terminal(terminal_entry.get())).grid(column=0, row=6)
+    terminal_entry.grid(row=9, column=0)
+    tkinter.Button(window, text="Add terminal", fg="green",
+                   command=lambda: add_new_terminal(terminal_entry.get())).grid(column=0, row=10)
 
-    tkinter.Label(window, text="Add new card", font=("Courier", 8), fg="blue").grid(row=3, column=1)
-    tkinter.Label(window, text="Card ID will be\ngenerated randomly", font=("Courier", 8)).grid(row=4,
-                                                                                                column=1)
-    tkinter.Button(window, text="Add card",
-                   command=lambda: add_new_card()).grid(row=5, column=1)
-
-    tkinter.Label(window, text="Add new worker", font=("Courier", 8), fg="blue").grid(row=3, column=2)
-    tkinter.Label(window, text="Insert new worker first name: ", font=("Courier", 8)).grid(row=4,
-                                                                                           column=2)
-
-    first_name_entry = tkinter.Entry(window)
-    first_name_entry.grid(row=5, column=2)
-    tkinter.Label(window, text="Insert new worker last name: ", font=("Courier", 8)).grid(row=6, column=2)
-    last_name_entry = tkinter.Entry(window)
-    last_name_entry.grid(row=7, column=2)
-    tkinter.Button(window, text="Add worker",
-                   command=lambda: add_new_worker(first_name_entry.get(), last_name_entry.get())).grid(column=2, row=8)
+    tkinter.Label(window, text="Remove marked terminal\nfrom system", font=("Courier", 10), fg="red").grid(row=12,
+                                                                                                           column=0)
+    tkinter.Button(window, text="Remove marked terminal", fg="red",
+                   command=lambda: remove_terminal(list_of_terminals.get(tkinter.ANCHOR).split(" - ")[0])).grid(
+        column=0, row=13)
 
     # --------------------------------------------------------------------------------------------------------
 
-    tkinter.Label(window, text="Connect or disconnect\nchosen terminal", font=("Courier", 10), fg="blue").grid(
-        row=10, column=0)
-    tkinter.Button(window, text="Connect",
-                   command=lambda: connect_terminal(list_of_terminals.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        row=11, column=0)
-    tkinter.Button(window, text="Disconnect",
-                   command=lambda: disconnect_terminal(list_of_terminals.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        row=12, column=0)
-
-    tkinter.Label(window, text="Manage card assignment:", font=("Courier", 10), fg="blue").grid(
-        row=10, column=1)
-    tkinter.Button(window, text="Delete chosen card assignment",
+    tkinter.Label(window, text="Manage card assignment:", font=("Courier", 10), fg="blue").grid(row=3, column=1)
+    tkinter.Button(window, text="Delete chosen card assignment", fg="blue",
                    command=lambda: remove_card_assignment(list_of_cards.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        row=11,
-        column=1)
-    tkinter.Button(window, text="Assign chosen card to chosen worker",
+        row=4, column=1)
+    tkinter.Button(window, text="Assign chosen card to chosen worker", fg="blue",
                    command=lambda: assign_card(list_of_workers.get(tkinter.ANCHOR).split(" - ")[0],
                                                list_of_cards.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        row=12, column=1)
-    tkinter.Button(window, text="Mark chosen card as stolen",
+        row=5, column=1)
+    tkinter.Button(window, text="Mark chosen card as stolen", fg="blue",
                    command=lambda: mark_card_as_stolen(list_of_cards.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        row=13, column=1)
+        row=6, column=1)
 
-    tkinter.Label(window, text="Create work time csv file", font=("Courier", 10), fg="blue").grid(row=10,
-                                                                                                  column=2)
-    tkinter.Button(window, text="Create csv for chosen worker",
-                   command=lambda: reports_handling.create_csv(
-                       list_of_workers.get(tkinter.ANCHOR).split(" - ")[0])).grid(row=11,
-                                                                                  column=2)
+    tkinter.Label(window, text="Add new card", font=("Courier", 10), fg="green").grid(row=8, column=1)
+    tkinter.Label(window, text="Card ID will be\ngenerated randomly", font=("Courier", 8), fg="green").grid(row=9,
+                                                                                                            column=1)
+    tkinter.Button(window, text="Add card", fg="green", command=lambda: add_new_card()).grid(row=10, column=1)
+
+    tkinter.Label(window, text="Remove marked card\nfrom system", font=("Courier", 10), fg="red").grid(row=12, column=1)
+    tkinter.Button(window, text="Remove marked card", fg="red",
+                   command=lambda: remove_card(list_of_cards.get(tkinter.ANCHOR).split(" - ")[0])).grid(
+        column=1, row=13)
+
     # --------------------------------------------------------------------------------------------------------
 
-    tkinter.Label(window, text="Remove marked terminal", font=("Courier", 8), fg="blue").grid(row=14, column=0)
-    tkinter.Button(window, text="Remove terminal",
-                   command=lambda: remove_terminal(list_of_terminals.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        column=0, row=15)
+    tkinter.Label(window, text="Create work time csv file", font=("Courier", 10), fg="blue").grid(row=3, column=2)
+    tkinter.Button(window, text="Create csv for chosen worker", fg="blue",
+                   command=lambda: reports_handling.create_csv(
+                       list_of_workers.get(tkinter.ANCHOR).split(" - ")[0])).grid(row=4, column=2)
 
-    tkinter.Label(window, text="Remove marked card", font=("Courier", 8), fg="blue").grid(row=14, column=1)
-    tkinter.Button(window, text="Remove card",
-                   command=lambda: remove_card(list_of_cards.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        column=1, row=15)
+    tkinter.Label(window, text="Add new worker", font=("Courier", 10), fg="green").grid(row=6, column=2)
+    tkinter.Label(window, text="Insert new worker first name: ", font=("Courier", 8), fg="green").grid(row=7, column=2)
 
-    tkinter.Label(window, text="Remove marked worker", font=("Courier", 8), fg="blue").grid(row=14, column=2)
-    tkinter.Button(window, text="Remove worker",
+    first_name_entry = tkinter.Entry(window)
+    first_name_entry.grid(row=8, column=2)
+    tkinter.Label(window, text="Insert new worker last name: ", font=("Courier", 10), fg="green").grid(row=9, column=2)
+    last_name_entry = tkinter.Entry(window)
+    last_name_entry.grid(row=10, column=2)
+    tkinter.Button(window, text="Add worker", fg="green",
+                   command=lambda: add_new_worker(first_name_entry.get(), last_name_entry.get())).grid(column=2, row=11)
+
+    tkinter.Label(window, text="Remove marked worker\nfrom system", font=("Courier", 10), fg="red").grid(row=13,
+                                                                                                         column=2)
+    tkinter.Button(window, text="Remove marked worker", fg="red",
                    command=lambda: remove_worker(list_of_workers.get(tkinter.ANCHOR).split(" - ")[0])).grid(
-        column=2, row=15)
+        column=2, row=14)
 
 
 if __name__ == "__main__":
